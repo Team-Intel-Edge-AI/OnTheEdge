f8087db0cf866f5f5dd1885cb2f2a48777f9caca
Yuriel849 <yuriel849sp@gmail.com>
2024-01-08 16:02:45 +0900

    Separate classes from main.py into separate files

---
+++ dev/combined/face_drawer.py
@@ -0,0 +1,74 @@
+"""
+Face drawer class.
+"""
+
+import cv2
+
+
+class FaceDrawer:
+    """
+    Draw over a given image of a human face.
+    """
+    @staticmethod
+    def draw_face(frame, xmin, ymin, xmax, ymax, text,
+                  landmarks, output_transform, emotion_image):
+        """
+        Draw emoticons over a detected human face.
+        인식한 얼굴 위에 이모티콘 이미지를 붙인다.
+        """
+        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 220, 0), 2)
+
+        for point in landmarks:
+            x = xmin + output_transform.scale((xmax - xmin) * point[0])
+            y = ymin + output_transform.scale((ymax - ymin) * point[1])
+            cv2.circle(frame, (int(x), int(y)), 1, (0, 255, 255), 2)
+
+        textsize = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 1)[0]
+        cv2.rectangle(frame, (xmin, ymin),
+                      (xmin + textsize[0], ymin - textsize[1]),
+                      (255, 255, 255), cv2.FILLED)
+        cv2.putText(frame, text, (xmin, ymin),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 1)
+
+        # 감정 이미지를 얼굴 영역의 높이에 맞추어 크기 조절
+        emotion_image_resized = cv2.resize(emotion_image,
+                                           (int(xmax - xmin),
+                                            int(ymax - ymin)))
+
+        # 이미지 합성
+        frame = FaceDrawer.overlay_transparent(frame, emotion_image_resized,
+                                               x=xmin, y=ymin)
+
+        # 이모티콘을 붙인 얼굴 프레임
+        return frame
+
+    # 투명한 배경을 그대로 이용하기 위해서 알파값을 살려 투명한 부분에 오버레이되게 한다.
+    @staticmethod
+    def overlay_transparent(background, overlay, x, y):
+        """
+        Overlay a transparent image onto a background.
+
+        Parameters:
+        - background: Background image.
+        - overlay: Overlay image with transparency (RGBA format).
+        - x, y: Coordinates to place the overlay image.
+
+        Returns:
+        - Resulting image after overlay.
+        """
+        background = background.copy()
+
+        # Extract the alpha channel from the overlay image
+        overlay_alpha = overlay[:, :, 3] / 255.0
+
+        # Calculate the inverse of the alpha channel
+        background_alpha = 1.0 - overlay_alpha
+
+        # Calculate the weighted sum of the background and overlay images
+        for c in range(0, 3):
+            background[y:y + overlay.shape[0], x:x + overlay.shape[1], c] = \
+                (background_alpha * background[y:y + overlay.shape[0],
+                                               x:x + overlay.shape[1], c] +
+                 overlay_alpha * overlay[:, :, c])
+
+        return background
---
+++ dev/combined/frame_processor.py
@@ -0,0 +1,89 @@
+"""
+Class to process video frames and apply face detection and recognition ML/AI models.
+
+동영상 프레임을 처리하여 얼굴 인식 머신러닝/인공지능 모델을 적용하는 클래스.
+"""
+
+import logging as log
+from openvino.runtime import Core, get_version
+from utils import crop
+from landmarks_detector import LandmarksDetector
+from face_detector import FaceDetector
+from faces_database import FacesDatabase
+from face_identifier import FaceIdentifier
+
+
+class FrameProcessor:
+    """
+    Class to process video frames.
+    """
+    QUEUE_SIZE = 16
+    blur_value = 20
+
+    def __init__(self, args):
+        self.allow_grow = args.allow_grow and not args.no_show
+
+        log.info('OpenVINO Runtime')
+        log.info('\tbuild: %s', get_version())
+        core = Core()
+
+        self.face_detector = FaceDetector(core, args.m_fd,
+                                          args.fd_input_size,
+                                          confidence_threshold=args.t_fd,
+                                          roi_scale_factor=args.exp_r_fd)
+        self.landmarks_detector = LandmarksDetector(core, args.m_lm)
+        self.face_identifier = FaceIdentifier(core, args.m_reid,
+                                              match_threshold=args.t_id,
+                                              match_algo=args.match_algo)
+
+        self.face_detector.deploy(args.d_fd)
+        self.landmarks_detector.deploy(args.d_lm, self.QUEUE_SIZE)
+        self.face_identifier.deploy(args.d_reid, self.QUEUE_SIZE)
+
+        log.debug('Building faces database using images from %s', args.fg)
+        self.faces_database = FacesDatabase(args.fg, self.face_identifier,
+                                            self.landmarks_detector,
+                                            self.face_detector
+                                            if args.run_detector
+                                            else None, args.no_show)
+        self.face_identifier.set_faces_database(self.faces_database)
+        log.info('Database is built, registered %s identities',
+                 len(self.faces_database))
+
+    def process(self, frame):
+        """
+        Process a video frame.
+        """
+        orig_image = frame.copy()
+
+        rois = self.face_detector.infer((frame,))
+        if self.QUEUE_SIZE < len(rois):
+            log.warning('Too many faces for processing. \
+                        Will be processed only %s of %s',
+                        self.QUEUE_SIZE, len(rois))
+            rois = rois[:self.QUEUE_SIZE]
+
+        landmarks = self.landmarks_detector.infer((frame, rois))
+        face_identities, unknowns = self.face_identifier.infer((frame,
+                                                                rois,
+                                                                landmarks))
+        if self.allow_grow and len(unknowns) > 0:
+            for i in unknowns:
+                # This check prevents saving half-images in image boundaries
+                if rois[i].position[0] == 0.0 or \
+                    rois[i].position[1] == 0.0 or \
+                        (rois[i].position[0] + rois[i].size[0] >
+                         orig_image.shape[1]) or \
+                        (rois[i].position[1] + rois[i].size[1] >
+                         orig_image.shape[0]):
+                    continue
+                crop_image = crop(orig_image, rois[i])
+                name = self.faces_database.ask_to_save(crop_image)
+                if name:
+                    ident = self.faces_database.dump_faces(crop_image,
+                                                           ace_identities[i].
+                                                           descriptor,
+                                                           name)
+                    face_identities[i].id = ident
+
+        return [rois, landmarks, face_identities]
