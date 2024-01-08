9c280317651970bf002effc7b02d371562e48391
Yuriel849 <yuriel849sp@gmail.com>
2024-01-08 16:06:09 +0900

    Delete unnecessary files

--- dev/controller.py
+++
@@ -1,8 +0,0 @@
-# The controller which manages the video input and output and AI models
-
-from cur_models import CommonModel
-
-
-if __name__ == "__main__":
-    model = CommonModel()
-    model.run()
